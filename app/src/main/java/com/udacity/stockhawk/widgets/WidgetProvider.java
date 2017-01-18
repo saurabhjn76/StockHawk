6b01c3ece0cf12ca69f0f209633cbc7f4f7341d1
saurabhjn76 <saurabhjn76@gmail.com>
2017-01-18 09:53:26 +0530

    Completed dummy data widget

--- app/src/main/java/com/udacity/stockhawk/widgets/WidgetProvider.java
+++ app/src/main/java/com/udacity/stockhawk/widgets/WidgetProvider.java
@@ -1,24 +1,76 @@
 package com.udacity.stockhawk.widgets;
 
 /**
- * Created by saurabh on 18/1/17.
+ * Created by saurabh on 18/1/17.THanks to http://dharmangsoni.blogspot.in/2014/03/collection-widget-with-event-handling.html
  */
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.annotation.SuppressLint;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
+import android.widget.Toast;
+
+import com.udacity.stockhawk.R;
 
 public class WidgetProvider extends AppWidgetProvider {
+    public static final String ACTION_TOAST = "com.stockhawk.widgets.ACTION_TOAST";
+    public static final String EXTRA_STRING = "com.stockhawk.widgets.EXTRA_STRING";
 
     @Override
     public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(ACTION_TOAST)) {
+            String item = intent.getExtras().getString(EXTRA_STRING);
+            Toast.makeText(context, item, Toast.LENGTH_LONG).show();
+        }
         super.onReceive(context, intent);
     }
 
+    @SuppressLint("NewApi")
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager,
                          int[] appWidgetIds) {
+        for (int widgetId : appWidgetIds) {
+            RemoteViews mView = initViews(context, appWidgetManager, widgetId);
+
+            // Adding collection list item handler
+            final Intent onItemClick = new Intent(context, WidgetProvider.class);
+            onItemClick.setAction(ACTION_TOAST);
+            onItemClick.setData(Uri.parse(onItemClick
+                    .toUri(Intent.URI_INTENT_SCHEME)));
+            final PendingIntent onClickPendingIntent = PendingIntent
+                    .getBroadcast(context, 0, onItemClick,
+                            PendingIntent.FLAG_UPDATE_CURRENT);
+            mView.setPendingIntentTemplate(R.id.widgetCollectionList,
+                    onClickPendingIntent);
+
+            appWidgetManager.updateAppWidget(widgetId, mView);
+        }
         super.onUpdate(context, appWidgetManager, appWidgetIds);
     }
+
+    @SuppressWarnings("deprecation")
+    @SuppressLint("NewApi")
+    private RemoteViews initViews(Context context,
+                                  AppWidgetManager widgetManager, int widgetId) {
+
+        RemoteViews mView = new RemoteViews(context.getPackageName(),
+                R.layout.widget_provider_layout);
+
+        Intent intent = new Intent(context, WidgetService.class);
+        intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
+
+        intent.setData(Uri.parse(intent.toUri(Intent.URI_INTENT_SCHEME)));
+        mView.setRemoteAdapter(widgetId, R.id.widgetCollectionList, intent);
+
+        return mView;
+    }
+
 }
--- app/src/main/java/com/udacity/stockhawk/widgets/WidgetService.java
+++ app/src/main/java/com/udacity/stockhawk/widgets/WidgetService.java
@@ -1,8 +1,22 @@
 package com.udacity.stockhawk.widgets;
 
 /**
- * Created by saurabh on 18/1/17.
+ * Created by saurabh on 18/1/17.THanks to http://dharmangsoni.blogspot.in/2014/03/collection-widget-with-event-handling.html
  */
 
-public class WidgetService {
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+@SuppressLint("NewApi")
+public class WidgetService extends RemoteViewsService {
+
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+
+        WidgetDataProvider dataProvider = new WidgetDataProvider(
+                getApplicationContext(), intent);
+        return dataProvider;
+    }
+
 }
